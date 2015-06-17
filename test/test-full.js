5a13ee1734e6610abc5a1f459faf5e988f5c5a89
Vladimir Agafonkin <agafonkin@gmail.com>
2015-06-17 17:51:31 -0400

    test for duplicate points

--- test/test-full.js
+++ test/test-full.js
@@ -19,7 +19,28 @@ test('throws on invalid GeoJSON', function (t) {
 
 function testTiles(inputFile, expectedFile, maxZoom, maxPoints) {
     test('full tiling test: ' + inputFile, function (t) {
-        t.same(genTiles(getJSON(inputFile), maxZoom, maxPoints), getJSON(expectedFile));
+        var tiles = genTiles(getJSON(inputFile), maxZoom, maxPoints);
+        t.same(tiles, getJSON(expectedFile));
+
+        for (var i in tiles) {
+            var tile = tiles[i];
+
+            for (var j = 0; j < tile.length; j++) {
+                var geom = tile[j].geometry;
+
+                if (tile[j].type > 1) {
+                    for (var m = 0; m < geom.length; m++) {
+                        for (var n = 1; n < geom[m].length; n++) {
+                            var p1 = geom[m][n - 1];
+                            var p2 = geom[m][n];
+
+                            t.ok(p1[0] !== p2[0] || p1[1] !== p2[0], p1 + ' ' + p2);
+                        }
+                    }
+                }
+            }
+        }
+
         t.end();
     });
 }
